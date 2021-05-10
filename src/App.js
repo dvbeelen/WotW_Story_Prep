11d9b6cfb93a0825cccb9a5ecc5716aaaabe27a0
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-05-10 21:28:00 +0200

    Worked on text splitting

--- src/App.js
+++ src/App.js
@@ -9,22 +9,47 @@ class App extends Component {
     };
   }
 
+  textObject = {
+    title: "",
+    text: {}
+  }
+
   handleChange(event) {
     console.log(event.target.value)
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.textObject.title = event.target[0].value
+    this.cutStringsFromInput(event.target[1].value)
+    console.log(this.textObject)
+  }
+
+  cutStringsFromInput(text){
+    let sentenceCount = 0
+    while (text.length >= 0){
+      let sentence = text.split(". ")[0]
+      text.replace(sentence, "")
+      this.textObject.text[sentenceCount] = sentence
+    }
+  }
   render() {
     return (
       <div>
-        <form>
-          <label>Enter value : </label>
-          <input
-            id="textInput" 
-            type="textarea" 
-            name="textValue"
-            onChange={this.handleChange}
-          />
-          <input type="submit" />
+        <form onSubmit={this.handleSubmit}>
+          <table>
+            <tbody>
+              <tr>
+                <td><label htmlFor="title">Text title: </label></td>
+                <td><input type="text" id="title"></input></td>
+              </tr>
+              <tr>
+                <td><label htmlFor="textInput">Enter value: </label></td>
+                <td><input id="textInput" type="textarea" name="textValue"/></td>
+              </tr>
+            </tbody>
+          </table>
+          <input type="submit"/>
         </form>
       </div>
     );
